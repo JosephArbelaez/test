ecac24fa2165de0c17a977be26ba02ddf607ebe4
JosephArbelaez <josepharbelaez@gmail.com>
2021-05-28 16:16:48 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React, {Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Page from './components/Page/Page';
 
-class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      page: "home"
-    };
-  }
-
-  pageChange = (page) => {
-    this.setState({page: page});
-  }
-
-  render() {
-    const { page } = this.state;
-
-    return (
+function App() {
+  return (
       <div>
         <div className="App">
           <Page page = {page} pageChange={this.pageChange}/>
           </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
